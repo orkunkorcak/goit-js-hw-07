4758fc1581ec96ee901f67418dfaa0ee0b051c4c
your_name <orkunkorcak@gmail.com>
2024-12-18 14:34:14 +0300

    task-6 finished

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,42 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const input = document.querySelector(".numbers");
+const createbtn = document.querySelector(".create");
+const destroybtn = document.querySelector(".destroy");
+const boxes = document.querySelector("#boxes");
+
+function createBoxes(amount) {
+  let size = 30;
+  let box = [];
+  for (let i = 0; i <= amount; i++) {
+    const addDiv = document.createElement("div");
+    addDiv.style.width = `${size}px`;
+    addDiv.style.height = `${size}px`;
+    addDiv.style.backgroundColor = getRandomHexColor();
+    box.push(addDiv);
+    size += 10;
+  }
+  boxes.append(...box);
+  input.value = "";
+}
+function destroy() {
+  boxes.innerHTML = "";
+  
+}
+createbtn.addEventListener("click", () => {
+  const amount = Number(input.value);
+  if (amount >= 1 && amount <= 100) {
+    createBoxes(amount);
+  } else {
+    alert("You can only enter a number between 1 and 100.")
+  }
+}
+);
+destroybtn.addEventListener("click", () => {
+  destroy();
+}
+)
+
+
